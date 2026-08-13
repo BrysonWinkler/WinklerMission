79499bb58ec64120b090fe8f6bc9cd5bf8663028
BrysonWinkler <Brysondwinkler@gmail.com>
2026-08-13 12:46:15 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbw7XgNayqu3Minq3QJvvQ7bOOo08smBaQPSVMLALgfvMGh_VGmLYpu2idGTave0LSOZ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzmYgRY7upYBIHPNK9RPBwvbGgk1zdFc5e8UZ5ijKebP3bBVA9ISwyEobdFKVybewRv/exec";
